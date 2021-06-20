9b7e74283260539a8ab5e9f8a902f55a6cc3129c
Luke Dart <dartybh@live.com.au>
2021-06-20 14:27:20 +0930

    Add project files.

---
+++ PickRandomCards/CardPicker.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickRandomCards
+{
+    class CardPicker
+    {
+        private static Random random = new Random();
+
+        public static string[] pickSomeCards(int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+
+            for (int i = 0; i < numberOfCards; i++)
+            {
+                pickedCards[i] = RandomValue() + " of " + RandomSuit();
+            }
+
+            return pickedCards;
+        }
+
+        private static string RandomSuit()
+        {
+            int value = random.Next(1, 5);
+
+            switch (value)
+            {
+                case 1:
+                    return "Diamonds";
+
+                case 2:
+                    return "Hearts";
+
+                case 3:
+                    return "Spades";
+
+                default:
+                    return "Clubs";
+            }
+        }
+
+        private static string RandomValue()
+        {
+            int value = random.Next(1, 14);
+
+            switch (value)
+            {
+                case 1:
+                    return "Ace";
+
+                case 11:
+                    return "Jack";
+
+                case 12:
+                    return "Queen";
+
+                case 13:
+                    return "King";
+
+                default:
+                    return value.ToString();
+            }
+        }
+
+      
+    }
+}
---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PickRandomCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of cards to pick: ");
+            string value = Console.ReadLine();
+            
+            if (int.TryParse(value, out int numberOfCards) && Convert.ToInt32(value) >= 0)
+            {
+                foreach (string card in CardPicker.pickSomeCards(numberOfCards))
+                {
+                    Console.WriteLine(card);
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("Invalid input: Please use positive numerical values only.");
+            }
+        }
+    }
+}
